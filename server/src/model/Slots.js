fbb5af9b6c0f06600514121d02b156648f7d6bb2
root <root@debian-danielsharp01.localdomain>
2019-06-10 23:55:51 +0000

    Messed up Slot indices fixed

--- server/src/model/Slots.js
+++ server/src/model/Slots.js
@@ -7,7 +7,7 @@ const SlotSchema = new Schema({
   content: Schema.Types.Mixed
 });
 
-SlotSchema.index({ userId: 1, content: 1 }, { unique: true });
+SlotSchema.index({ userId: 1, name: 1 }, { unique: true });
 
 export const FilterSlot = appConnection.model("FilterSlot", SlotSchema);
 export const SortCriteriaSlot = appConnection.model("SortCriteriaSlot", SlotSchema);
