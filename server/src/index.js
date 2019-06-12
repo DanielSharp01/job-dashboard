de63daf02e40c491e96f9122b8ff011b27c0a628
DanielSharp01 <DanielSharp01@gmail.com>
2019-06-12 15:38:54 +0200

    Logout system, some style changes

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,13 @@ import bodyParser from "body-parser";
 import session from "express-session";
 import connectMongo from "connect-mongo";
 import cookieParser from "cookie-parser";
+import fs from "fs";
+import path from "path";
 import jobs from "./routes/jobs";
 import filterSlots from "./routes/filterSlots";
 import sortCriteriaSlots from "./routes/sortCriteriaSlots";
 import { accountsConnection } from "./db";
+import User from "./model/User";
 import auth from "./middlewares/auth";
 
 const port = 3100;
@@ -25,6 +28,16 @@ app.use(
 app.use(cookieParser());
 
 app.use(auth(true, true));
+
+app.get("/job-dashboard/", async (req, res, next) => {
+  let loggedInUser = await User.findOne({ _id: req.userId });
+  if (!loggedInUser) return next("There was an error!");
+  req.session.redirectUrl = req.originalUrl;
+
+  let html = fs.readFileSync(path.join(__dirname, "../public/index.html"), "utf8");
+  html = html.replace("<LOGGED IN USER>", loggedInUser.username.replace(/<.?script>/gi, ""));
+  res.send(html);
+});
 
 app.use("/job-dashboard", express.static("public"));
 
--- server/src/middlewares/jobs/parse.js
+++ server/src/middlewares/jobs/parse.js
@@ -3,44 +3,62 @@ import { parsePay } from "../../parserCommons";
 export default (req, res, next) => {
   for (let organization of req.organizations) {
     console.log(`Parsing ${organization} list`);
-    parseByOrg[organization](res);
+    parseByOrg[organization](res, next);
   }
 
   return next();
-}
+};
 
 const parseByOrg = [];
 
-parseByOrg["Schönherz"] = (res) => {
+parseByOrg["Schönherz"] = (res, next) => {
   if (!res.requestHtml["Schönherz"]) return next("Problem with request for Scönherz");
 
   const $ = res.requestHtml["Schönherz"];
-  $("body").children("div").each((i, jobDiv) => {
-    let pay = parsePay($(jobDiv).find(".sdcard-text h5").text());
+  $("body")
+    .children("div")
+    .each((i, jobDiv) => {
+      let pay = parsePay(
+        $(jobDiv)
+          .find(".sdcard-text h5")
+          .text()
+      );
 
-    let anchor = $(jobDiv).find(".text h4 a");
-    let name = anchor.text();
-    let link = anchor.attr("href");
-    let linkSpl = link.split("/");
-    let id = parseInt(linkSpl[linkSpl.length - 1].split("-")[0]);
+      let anchor = $(jobDiv).find(".text h4 a");
+      let name = anchor.text();
+      let link = anchor.attr("href");
+      let linkSpl = link.split("/");
+      let id = parseInt(linkSpl[linkSpl.length - 1].split("-")[0]);
 
-    res.jobs.push({ pay, name, link: "https://www.schonherz.hu" + link, id, organization: "Schönherz" });
-  });
-}
+      res.jobs.push({ pay, name, link: "https://www.schonherz.hu" + link, id, organization: "Schönherz" });
+    });
+};
 
-parseByOrg["Műisz"] = (res) => {
+parseByOrg["Műisz"] = (res, next) => {
   if (!res.requestHtml["Műisz"]) return next("Problem with request for Műisz");
 
   const $ = res.requestHtml["Műisz"];
   let jobs = [];
-  $("#Container>ul").children("div.budapest.informatikai").each((i, jobDiv) => {
-    let pay = parsePay($(jobDiv).find("div.munka_ber").text());
+  $("#Container>ul")
+    .children("div.budapest.informatikai")
+    .each((i, jobDiv) => {
+      let pay = parsePay(
+        $(jobDiv)
+          .find("div.munka_ber")
+          .text()
+      );
 
-    let link = $(jobDiv).find("a").eq(0).attr("href");
-    let linkSpl = link.split("/");
-    let id = parseInt(linkSpl[linkSpl.length - 1]);
-    let name = $(jobDiv).find("h3.list_e").text().trim();
+      let link = $(jobDiv)
+        .find("a")
+        .eq(0)
+        .attr("href");
+      let linkSpl = link.split("/");
+      let id = parseInt(linkSpl[linkSpl.length - 1]);
+      let name = $(jobDiv)
+        .find("h3.list_e")
+        .text()
+        .trim();
 
-    res.jobs.push({ pay, name, link: "https://www.muisz.hu/" + link, id, organization: "Műisz" })
-  });
-}
+      res.jobs.push({ pay, name, link: "https://www.muisz.hu/" + link, id, organization: "Műisz" });
+    });
+};
